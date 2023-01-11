ab609a2e8b940d1d5de326eceb7547476d2181be
root <Beach OS>
2023-01-11 08:10:15 +0000

    BeachOS

---
+++ beach_bluejay.mk
@@ -0,0 +1,22 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/beach/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/gs101/beach_common.mk)
+$(call inherit-product, device/google/bluejay/device-beach.mk)
+$(call inherit-product, device/google/bluejay/aosp_bluejay.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := beach_bluejay
+PRODUCT_MODEL := Pixel 6a
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bluejay \
+    PRIVATE_BUILD_DESC="bluejay-user 13 TQ1A.230105.001.A2 9325585 release-keys"
+
+BUILD_FINGERPRINT := google/bluejay/bluejay:13/TQ1A.230105.001.A2/9325585:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, vendor/google/bluejay/bluejay-vendor.mk)
